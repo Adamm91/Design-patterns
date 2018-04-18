7921e34cc95b49fc95c9499e922e78fb3f0060de
Adamm91 <adam.marcinkowski91@gmail.com>
2018-04-18 14:01:48 +0200

    add singleton

---
+++ src/main/java/singleton/Shop.java
@@ -0,0 +1,13 @@
+package singleton;
+
+public class Shop {
+
+    private static Shop instance;
+
+    public static Shop instanceOf () {
+        if (instance == null) {
+            instance = new Shop();
+        }
+        return instance;
+    }
+}
